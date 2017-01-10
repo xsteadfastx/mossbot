8806564aa109948c111190faed6b83bf0ae3a10c
Marvin Steadfast <marvin@xsteadfastx.org>
2017-01-10 15:35:47 +0100

    test for `on_message`

--- test_mossbot.py
+++ test_mossbot.py
@@ -2,6 +2,8 @@ import pytest
 
 from unittest import mock
 
+from matrix_client.room import Room
+
 import mossbot
 
 
@@ -54,3 +56,125 @@ def test_url_exception(requests_mock):
 
     with pytest.raises(Exception):
         assert mossbot.MOSS.serve('http://foo.bar') == ('skip', None)
+
+
+@mock.patch('mossbot.MOSS', autospec=True)
+@mock.patch('mossbot.config')
+def test_on_message_nothing(config_mock, moss_mock):
+    event = {
+        'content': {},
+        'sender': {},
+    }
+
+    room_mock = mock.Mock(spec=Room)
+
+    mossbot.on_message(room_mock, event)
+
+    room_mock.assert_not_called()
+
+
+@mock.patch('mossbot.MOSS', autospec=True)
+@mock.patch('mossbot.config')
+def test_on_message_text(config_mock, moss_mock):
+    event = {
+        'content': {
+            'msgtype': 'm.text',
+            'body': 'Foo Bar'
+        },
+        'sender': {'@bar:foo.tld'},
+    }
+
+    msg = (
+        'text',
+        'Foo Bar'
+    )
+
+    config_mock.UID = '@foo:bar.tld'
+    moss_mock.serve.return_value = msg
+    room_mock = mock.Mock(spec=Room)
+
+    mossbot.on_message(room_mock, event)
+
+    room_mock.send_text.assert_called_with('Foo Bar')
+
+
+@mock.patch('mossbot.MOSS', autospec=True)
+@mock.patch('mossbot.config')
+def test_on_message_notice(config_mock, moss_mock):
+    event = {
+        'content': {
+            'msgtype': 'm.text',
+            'body': 'Foo Bar'
+        },
+        'sender': {'@bar:foo.tld'},
+    }
+
+    msg = (
+        'notice',
+        'Foo Bar'
+    )
+
+    config_mock.UID = '@foo:bar.tld'
+    moss_mock.serve.return_value = msg
+    room_mock = mock.Mock(spec=Room)
+
+    mossbot.on_message(room_mock, event)
+
+    room_mock.send_notice.assert_called_with('Foo Bar')
+
+
+@mock.patch('mossbot.MOSS', autospec=True)
+@mock.patch('mossbot.config')
+def test_on_message_html(config_mock, moss_mock):
+    event = {
+        'content': {
+            'msgtype': 'm.text',
+            'body': 'Foo Bar'
+        },
+        'sender': {'@bar:foo.tld'},
+    }
+
+    msg = (
+        'html',
+        'Foo Bar'
+    )
+
+    config_mock.UID = '@foo:bar.tld'
+    moss_mock.serve.return_value = msg
+
+    room_mock = mock.Mock()
+    room_mock.room_id = '!foobar:foo.tld'
+    room_mock.client.api.get_html_body.return_value = 'HTML'
+
+    mossbot.on_message(room_mock, event)
+
+    room_mock.client.api.send_message_event.assert_called_with(
+        '!foobar:foo.tld',
+        'm.room.message',
+        'HTML'
+    )
+
+
+@mock.patch('mossbot.MOSS', autospec=True)
+@mock.patch('mossbot.config')
+def test_on_message_skip(config_mock, moss_mock):
+    event = {
+        'content': {
+            'msgtype': 'm.text',
+            'body': 'Foo Bar'
+        },
+        'sender': {'@bar:foo.tld'},
+    }
+
+    msg = (
+        'skip',
+        None
+    )
+
+    config_mock.UID = '@foo:bar.tld'
+    moss_mock.serve.return_value = msg
+    room_mock = mock.Mock(spec=Room)
+
+    mossbot.on_message(room_mock, event)
+
+    room_mock.assert_not_called()
